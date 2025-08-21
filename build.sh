49ca864a5f78a7a3241737558db030ca63963ee4
Ben Wang <benwangott@gmail.com>
2025-08-21 02:12:51 -0400

    used toggle in fuck.js to simplify things

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash 
-cat functionconstruct.js > nodefuck.js 
-echo \( >> nodefuck.js 
-./fuck.js server.js >> nodefuck.js 
-echo \) >> nodefuck.js 
-echo \( >> nodefuck.js 
-echo \) >> nodefuck.js 
-echo -n $(tr -d '\n' < nodefuck.js) > nodefuck.js
