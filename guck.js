adf4a8c99366b439a1ac2b53c8332370bb52a57e
Ben Wang <benwangott@gmail.com>
2025-08-22 17:13:16 -0400

    clean up helper functions

--- guck.js
+++ guck.js
@@ -25,21 +25,26 @@ const SIMPLE = {
     'true': '!![]',
     'undefined': '[][[]]',
     'NaN': '+[![]]',
-    'Infinity': '+(+!![]+(!![]+[])[+!![]+!![]+!![]]+[+!![]]+[+[]]+[+[]]+[+[]])'
+    'Infinity': '+(+!![]+(!![]+[])[+!![]+!![]+!![]]+[+!![]]+[+[]]+[+[]]+[+[]])',
 }
-Object.assign(SIMPLE, fuckedNumbers);
+Object.assign(SIMPLE, fuckedNumbers); // might remove
+
 // helper functions
 // get SIMPLE's values
 // append +[] to them
 // find the key with the smallest index where the character occurs
-const keyIndexFinder = (c) => Object.fromEntries(Object.keys(SIMPLE).filter((k) => k.includes(c)).map((s) => [s.indexOf(c), s])); // returns empty object if not found
-const minIndexFinder = (c) => Object.keys(keyIndexFinder(c)).reduce((a,b) => Math.min(a,b)); 
-const keyFinder = (c) => keyIndexFinder(c)[minIndexFinder(c)];
-// TODO: break pipeline after the filter to do a separate check for chars in none of the simple words
-// the sauce
-const mapping = (c) => `(${SIMPLE[keyFinder(c)]}+[])[${fuckedNumbers[minIndexFinder(c)]}]`;
+// TODO: get rid of implicit dependence on SIMPLE and pass it or something else as an argument in order to deal with things not in simple
+const debugChar = '1';
+const keyIndexFinder = (c) => Object.fromEntries(Object.keys(SIMPLE).filter((k) => k.includes(c)).map((s) => [s.indexOf(c), s]))
+const indexFinder = (c) => Object.keys(keyIndexFinder(c)); // returns empty [] if not found
+const keyFinder = (c) => Object.values(keyIndexFinder(c));
+const minIndexFinder = (c) => ((indexFinder(c)).length !== 0) ? indexFinder(c).reduce((a,b) => Math.min(a,b)) : []; 
+const minKeyFinder= (c) => keyFinder(c)[indexFinder(c).indexOf(`${minIndexFinder(c)}`)];
 
+// the sauce - FIX SIMPLE PASS FOR NUMBERS
+const mapping = (c) => (isNaN(c)) ? `(${SIMPLE[minKeyFinder(c)]}+[])[${fuckedNumbers[minIndexFinder(c)]}]` : `[${fuckedNumbers[c]}]`;
 // test mapping function's encoding of numbers
-const exampleFileLine = 'ad123';
+const exampleFileLine = '1111';
 const mapLn = (str) => str.split('').map((c) => mapping(c)).join('+'); // also the sauce
 console.log(mapLn(exampleFileLine), Function(`return ${mapLn(exampleFileLine)}`)());
+// console.log([indexFinder, keyFinder, minIndexFinder, minKeyFinder, mapping].map(f => f(debugChar)));
